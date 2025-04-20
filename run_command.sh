f9c80be14fe3c32a59195d6831870ec016e1e352
Naghmeh Farzi <nf1104@usnh.edu>
2025-04-19 22:56:12 -0400

    dl23 added

--- run_command.sh
+++ run_command.sh
@@ -1,9 +1,14 @@
+# --prompt_mode can be changed to "zeroshot_basic" if needed
+
+#-------------------------------Meta-Llama-3-8B-Instruct--------------------------------------
+
+
 # python src/main.py \
 #   --model_id "meta-llama/Meta-Llama-3-8B-Instruct" \
 #   --test_qrel_path "./data/dl2023/llm4eval_test_qrel_2024_withRel.txt" \
 #   --queries_path "./data/dl2023/llm4eval_query_2024.txt" \
 #   --docs_path "./data/dl2023/llm4eval_document_2024.jsonl" \
-#   --prompt_mode "fewshot_bing" \
+#   --prompt_mode "zeroshot_bing" \
 #   --result_file_path "./results/dl23_test_fewshot_bing_Llama-3-8B-Instruct.txt" \
 
 
@@ -12,7 +17,7 @@
 #   --test_qrel_path "./data/dl2019/2019qrels-pass.txt" \
 #   --queries_path "./data/dl2019/msmarco-test2019-queries.tsv" \
 #   --docs_path "./data/dl2019/dl2019_document.jsonl" \
-#   --prompt_mode "zeroshot_basic" \
+#   --prompt_mode "zeroshot_bing" \
 #   --result_file_path "./results/dl19_test_zeroshot_basic_Llama-3-8B-Instruct.txt" \
 #   # --max_pair 1
 
@@ -22,41 +27,41 @@
 #   --test_qrel_path "./data/dl2020/2020qrels-pass.txt" \
 #   --queries_path "./data/dl2020/msmarco-test2020-queries.tsv" \
 #   --docs_path "./data/dl2020/dl2020_document.jsonl" \
-#   --prompt_mode "zeroshot_basic" \
+#   --prompt_mode "zeroshot_bing" \
 #   --result_file_path "./results/dl20_test_zeroshot_basic_Llama-3-8B-Instruct.txt" \
 
-#----------------------------------------------------------------------------------------
+#----------------------------------flan-t5-large---------------------------------------
 
 
-python src/main.py \
-  --model_id "google/flan-t5-large" \
-  --test_qrel_path "./data/dl2023/llm4eval_test_qrel_2024_withRel.txt" \
-  --queries_path "./data/dl2023/llm4eval_query_2024.txt" \
-  --docs_path "./data/dl2023/llm4eval_document_2024.jsonl" \
-  --prompt_mode "zeroshot_bing" \
-  --result_file_path "./results/dl23_test_zeroshot_bing_flan-t5-large.txt" \
+# python src/main.py \
+#   --model_id "google/flan-t5-large" \
+#   --test_qrel_path "./data/dl2023/llm4eval_test_qrel_2024_withRel.txt" \
+#   --queries_path "./data/dl2023/llm4eval_query_2024.txt" \
+#   --docs_path "./data/dl2023/llm4eval_document_2024.jsonl" \
+#   --prompt_mode "zeroshot_bing" \
+#   --result_file_path "./results/dl23_test_zeroshot_bing_flan-t5-large.txt" \
 
 
-python src/main.py \
-  --model_id "google/flan-t5-large" \
-  --test_qrel_path "./data/dl2019/2019qrels-pass.txt" \
-  --queries_path "./data/dl2019/msmarco-test2019-queries.tsv" \
-  --docs_path "./data/dl2019/dl2019_document.jsonl" \
-  --prompt_mode "zeroshot_bing" \
-  --result_file_path "./results/dl19_test_zeroshot_bing_flan-t5-large.txt" \
+# python src/main.py \
+#   --model_id "google/flan-t5-large" \
+#   --test_qrel_path "./data/dl2019/2019qrels-pass.txt" \
+#   --queries_path "./data/dl2019/msmarco-test2019-queries.tsv" \
+#   --docs_path "./data/dl2019/dl2019_document.jsonl" \
+#   --prompt_mode "zeroshot_bing" \
+#   --result_file_path "./results/dl19_test_zeroshot_bing_flan-t5-large.txt" \
 
 
-python src/main.py \
-  --model_id "google/flan-t5-large" \
-  --test_qrel_path "./data/dl2020/2020qrels-pass.txt" \
-  --queries_path "./data/dl2020/msmarco-test2020-queries.tsv" \
-  --docs_path "./data/dl2020/dl2020_document.jsonl" \
-  --prompt_mode "zeroshot_bing" \
-  --result_file_path "./results/dl20_test_zeroshot_bing_flan-t5-large.txt" \
+# python src/main.py \
+#   --model_id "google/flan-t5-large" \
+#   --test_qrel_path "./data/dl2020/2020qrels-pass.txt" \
+#   --queries_path "./data/dl2020/msmarco-test2020-queries.tsv" \
+#   --docs_path "./data/dl2020/dl2020_document.jsonl" \
+#   --prompt_mode "zeroshot_bing" \
+#   --result_file_path "./results/dl20_test_zeroshot_bing_flan-t5-large.txt" \
 
 
 
-#----------------------------------------------------------------------------------------
+#---------------------------------------DeepSeek-V3-------------------------------------------------
 # python src/main.py \
 #   --model_id "deepseek-ai/DeepSeek-V3" \
 #   --test_qrel_path "./data/dl2023/llm4eval_test_qrel_2024_withRel.txt" \
@@ -84,3 +89,53 @@ python src/main.py \
   # --prompt_mode "zeroshot_bing" \
   # --result_file_path "./results/dl20_test_zeroshot_bing_DSV3.txt" \
   # -together
+
+
+
+
+
+#---------------------------------------Llama-3.3-70B-Instruct-Turbo-------------------------------------------------
+
+
+
+
+
+python src/main.py \
+  --model_id "meta-llama/Llama-3.3-70B-Instruct-Turbo" \
+  --test_qrel_path "./data/dl2023/llm4eval_test_qrel_2024_withRel.txt" \
+  --queries_path "./data/dl2023/llm4eval_query_2024.txt" \
+  --docs_path "./data/dl2023/llm4eval_document_2024.jsonl" \
+  --prompt_mode "zeroshot_bing" \
+  --result_file_path "./results/dl23_test_zeroshot_bing_Llama-3.3-70B.txt" \
+  -together
+  # --max_pair 1
+
+
+
+
+python src/main.py \
+  --model_id "meta-llama/Llama-3.3-70B-Instruct-Turbo" \
+  --test_qrel_path "./data/dl2019/2019qrels-pass.txt" \
+  --queries_path "./data/dl2019/msmarco-test2019-queries.tsv" \
+  --docs_path "./data/dl2019/dl2019_document.jsonl" \
+  --prompt_mode "zeroshot_bing" \
+  --result_file_path "./results/dl19_test_zeroshot_bing_Llama-3.3-70B.txt" \
+  -together
+  # --max_pair 1
+
+
+
+
+python src/main.py \
+  --model_id "meta-llama/Llama-3.3-70B-Instruct-Turbo" \
+  --test_qrel_path "./data/dl2020/2020qrels-pass.txt" \
+  --queries_path "./data/dl2020/msmarco-test2020-queries.tsv" \
+  --docs_path "./data/dl2020/dl2020_document.jsonl" \
+  --prompt_mode "zeroshot_bing" \
+  --result_file_path "./results/dl20_test_zeroshot_bing_Llama-3.3-70B.txt" \
+  -together
+#   --max_pair 1
+
+
+
+
